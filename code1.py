a57374230693050210e249945c911a6c3dfe6db5
siddhant jadhav <siddhantj444@gmail.com>
2020-10-14 18:49:18 +0530

    this is a DEMO code

--- code1.py
+++ code1.py
@@ -4,3 +4,9 @@ for number in range(0,10):
         print("number is even",number)
     else:
         print("number is odd",number)
+
+input=char("Enter your Name")
+if 'Name'=="sid":
+        print("welcome!")
+else:
+        print("who dis ?")
